2cfd55935a23be05a6af1867cc5a17bee3a46938
Tom Fletcher <signups@tomfletcher.me>
2019-01-26 12:35:37 +0000

    Window Events

--- Impetus/src/Impetus/Application.cpp
+++ Impetus/src/Impetus/Application.cpp
@@ -1,17 +1,18 @@
 #include "impch.h"
 #include "Application.h"
 
-#include "Impetus/Events/ApplicationEvent.h"
 #include "Impetus/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Impetus {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
-
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 
@@ -27,5 +28,18 @@ namespace Impetus {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+	void Application::OnEvent(Event & e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		IMPETUS_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Impetus/src/Impetus/Application.h
+++ Impetus/src/Impetus/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Window.h"
+#include "Impetus/Events/ApplicationEvent.h"
 
 namespace Impetus {
 
@@ -13,7 +14,11 @@ namespace Impetus {
 
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
